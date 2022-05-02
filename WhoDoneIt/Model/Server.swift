2baf2a55a5de1c1624ad7809f93357a45f6dffe8
Idris Mahamdi <73561935+idrismahamdi@users.noreply.github.com>
2022-05-02 15:48:06 +0100

    final version

--- WhoDoneIt/Model/Server.swift
+++ WhoDoneIt/Model/Server.swift
@@ -15,9 +15,6 @@ class Server {
     var clientArr: [TCPClient] = []
     var playerArr: [player] = []
     let playerCoreData = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    
-    
-
 
 /* Ping pong game - keeps connection alive between server and client */
     func ping(client: TCPClient) -> Bool? {
@@ -151,136 +148,133 @@ class Server {
         }
         return true
     }
-    
-    
-
 
 /* Main Server Function */
-func startServer(playerCount: Player) {
-    
-    /* Setting the server to run on 0.0.0.0 so it can be accessed locally by any device */
-    let server = TCPServer(address: "0.0.0.0", port: 8080)
-   
-    /*Start to listen on the server */
-       switch server.listen() {
-        /* if sucessful then start */
-       case .success:
-           t = Thread {
-                            
-               /* Call function to accept incoming connections
-                  if connection fails/player returns home : close server
-                  otherwise if game started setup the game */
-               if self.acceptingConnections(serverInfo: playerCount, server: server) == false{
-                   server.close()
-               } else{
-               
-               var playerNumberAssingment = 0, traitor = 0
+    func startServer(playerCount: Player) {
+        
+        /* Setting the server to run on 0.0.0.0 so it can be accessed locally by any device */
+        let server = TCPServer(address: "0.0.0.0", port: 8080)
+       
+        /*Start to listen on the server */
+           switch server.listen() {
+            /* if sucessful then start */
+           case .success:
+               t = Thread {
+                                
+                   /* Call function to accept incoming connections
+                      if connection fails/player returns home : close server
+                      otherwise if game started setup the game */
+                   if self.acceptingConnections(serverInfo: playerCount, server: server) == false{
+                       server.close()
+                   } else{
+                   
+                   var playerNumberAssingment = 0, traitor = 0
 
-               /* If there is at least one player, then randomly assign a player to become a traitor */
-               if self.clientArr.count != 0{
-                    traitor = Int.random(in: 0..<self.clientArr.count)}
-               
-               /* For each player that is connected to the server, run the gameStarted function
-                  Assings each player a playercount, and the correlating crewmember or traitor */
-               for i in self.clientArr {
-                   playerNumberAssingment += 1
-                   let p = player(client: i, playerNumber: playerNumberAssingment)
-                   self.playerArr.append(p)
-                   self.gameStarted(client: i, traitor: traitor, playerNumberAssingment: playerNumberAssingment)}
+                   /* If there is at least one player, then randomly assign a player to become a traitor */
+                   if self.clientArr.count != 0{
+                        traitor = Int.random(in: 0..<self.clientArr.count)}
                    
-                   
-               var traitorWin = 0, crewWin = 0, wasKilled = 0, gameOver = false, count = 0
-                   
-                   
-              /* Start server side game loop until the game has finished */
-               while gameOver == false{
-                   
-              /* Start of game loop */
-                   crewWin = 0
-                   traitorWin = 0
-                   count = 0
-            
-                   
-                   /* For each client that is connected to the server */
-                       for i in self.playerArr {
-                           
-                           if (i.playerNumber-1 != traitor) {
+                   /* For each player that is connected to the server, run the gameStarted function
+                      Assings each player a playercount, and the correlating crewmember or traitor */
+                   for i in self.clientArr {
+                       playerNumberAssingment += 1
+                       let p = player(client: i, playerNumber: playerNumberAssingment)
+                       self.playerArr.append(p)
+                       self.gameStarted(client: i, traitor: traitor, playerNumberAssingment: playerNumberAssingment)}
+                       
+                       
+                   var traitorWin = 0, crewWin = 0, wasKilled = 0, gameOver = false, count = 0
+                       
+                       
+                  /* Start server side game loop until the game has finished */
+                  /* Start of observer design pattern
+                   - by oberserving players actions within the game */
+                   while gameOver == false{
+                       
+                  /* Start of game loop */
+                       crewWin = 0
+                       traitorWin = 0
+                       count = 0
+                
+                       
+                       /* For each client that is connected to the server */
+                           for i in self.playerArr {
+                               
+                               if (i.playerNumber-1 != traitor) {
 
-                           /* Check if they have completed their tasks
-                              If they have completed their tasks then crewWin adds nothing on
-                              If they have not completed their tasks the crewWin adds 1 */
-                           crewWin = crewWin + self.tasksComplete(client: i.client)!
-                           
-                           /* check if they are dead */
-                           traitorWin = traitorWin + self.isDead(client: i.client)!
+                               /* Check if they have completed their tasks
+                                  If they have completed their tasks then crewWin adds nothing on
+                                  If they have not completed their tasks the crewWin adds 1 */
+                               crewWin = crewWin + self.tasksComplete(client: i.client)!
                                
-                           }
-                           
-                           /* checks for the traitor */
-                           if (i.playerNumber-1 == traitor) {
-                               
-                            /* check if the traitor killed someone
-                            If a player has been killed then send player that was killed that they were killed*/
-                            wasKilled = self.killed(client: i.client, wasKilled: wasKilled)!
-                            if (wasKilled > 0) {
-                                   for j in self.playerArr{
-                                       if j.playerNumber == wasKilled{
-                                           j.client.send(string: "die\(wasKilled)")
-                                       }}}
-                               
-                               /* If traitor has disconnected, echo out disconnection to all players and end game loop */
-                               if self.traitorAlive(client: i.client) == false{
-                                   for j in self.playerArr{
-                                       j.client.send(string: "winD")
-                                       gameOver = true
-                                   }
+                               /* check if they are dead */
+                               traitorWin = traitorWin + self.isDead(client: i.client)!
+                                   
                                }
                                
-                           }
-                           
-                           
-                           /* Ping to keep connection alive if false then remove client from players */
-                           if self.ping(client: i.client) == false {
-                               self.playerArr.remove(at: count)
-                              }
-                           count += 1
-                           }
-                                         
-                     
-                   /* If no crewmembers have any tasks left, echo out result to all players and end game loop */
-                        if (crewWin == 0){
-                               for j in self.playerArr{
-                                   j.client.send(string: "winC")
-                                   gameOver = true
-                               }}
-                   
-                   /* If no crewmembers alive, echo out result to all players and end game loop */
-                        if (traitorWin == 0){
-                                for j in self.playerArr{
-                                    j.client.send(string: "winT")
-                                    gameOver = true
-                                }}
+                               /* checks for the traitor */
+                               if (i.playerNumber-1 == traitor) {
+                                   
+                                /* check if the traitor killed someone
+                                If a player has been killed then send player that was killed that they were killed*/
+                                wasKilled = self.killed(client: i.client, wasKilled: wasKilled)!
+                                if (wasKilled > 0) {
+                                       for j in self.playerArr{
+                                           if j.playerNumber == wasKilled{
+                                               j.client.send(string: "die\(wasKilled)")
+                                           }}}
+                                   
+                                   /* If traitor has disconnected, echo out disconnection to all players and end game loop */
+                                   if self.traitorAlive(client: i.client) == false{
+                                       for j in self.playerArr{
+                                           j.client.send(string: "winD")
+                                           gameOver = true
+                                       }
+                                   }
+                                   
+                               }
+                               
+                               
+                               /* Ping to keep connection alive if false then remove client from players */
+                               if self.ping(client: i.client) == false {
+                                   self.playerArr.remove(at: count)
+                                  }
+                               count += 1
+                               }
+                                             
+                       /* If no crewmembers have any tasks left, echo out result to all players and end game loop */
+                            if (crewWin == 0){
+                                   for j in self.playerArr{
+                                       j.client.send(string: "winC")
+                                       gameOver = true
+                                   }}
                        
-                   /* End of game loop */
-               }
-                   
-                   /* When game ended (game loop stopped) close the server */
-                   server.close()}}
+                       /* If no crewmembers alive, echo out result to all players and end game loop */
+                            if (traitorWin == 0){
+                                    for j in self.playerArr{
+                                        j.client.send(string: "winT")
+                                        gameOver = true
+                                    }}
+                           
+                       /* End of game loop */
+                   }
+                       
+                       /* When game ended (game loop stopped) close the server */
+                       server.close()}}
+               
+               /* starting the server in a new thread */
+               /* concurrent work design pattern */
+                t.start()
            
-           /* starting the server in a new thread */
-            t.start()
-       
-           
-         /* If server did not start send debug error */
-         case .failure(let error):
-           print(error)}
+               
+             /* If server did not start send debug error */
+             case .failure(let error):
+               print(error)}
     
     
-}
+    }
     
-    
-    
-    /* closes server */
+/* closes server */
     func cancelServer(){
         let items = try? playerCoreData.fetch(ClientData.fetchRequest())
         let clientData = items![0]
@@ -295,8 +289,7 @@ func startServer(playerCount: Player) {
       
     }
     
-    
-    /* server data setup */
+/* server data setup */
     func defaultInfo() -> Player?{
     
         /* if first time using the app then set up core data else use the first item in array */
